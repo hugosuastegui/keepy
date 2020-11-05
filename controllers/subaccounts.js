7d5664491afb6e3e4f78efc7be13202c3f74d412
Hugo Suastegui <hugo.suastegui.pineda@gamil.com>
2020-11-05 16:25:06 -0600

    Completed all controllers, test pending

--- controllers/subaccounts.js
+++ controllers/subaccounts.js
@@ -6,6 +6,33 @@ exports.getAllSubaccounts = async (req, res, next) => {
   console.log(userSubaccounts);
   res.status(200).json({ userSubaccounts });
 };
-exports.createSubaccount = async (req, res, next) => {};
-exports.updateSubaccount = async (req, res, next) => {};
-exports.deleteSubaccount = async (req, res, next) => {};
+
+exports.createSubaccount = async (req, res, next) => {
+  const { name, account } = req.body;
+  const subaccount = await Subaccount.create({
+    name,
+    account,
+    user: req.user.id,
+  });
+  res.status(200).json({ subaccount });
+};
+
+exports.updateSubaccount = async (req, res, next) => {
+  const { name, account } = req.body;
+  const { subaccountId } = req.params;
+  const subaccount = await Subaccount.findOne({ _id: subaccountId });
+  if (typeof name !== undefined) {
+    subaccount.name = name;
+  }
+  if (typeof account !== undefined) {
+    subaccountId.account = account;
+  }
+  subaccount.save();
+  res.status(200).json({ message: "Subaccount updated sucessfully" });
+};
+
+exports.deleteSubaccount = async (req, res, next) => {
+  const { subaccountId } = req.params;
+  await Subaccount.findOneAndRemove({ _id: subaccountId });
+  res.status(200).json({ message: "Subaccount deleted succesfully" });
+};
