df10b083a0d874bbe5ededf4e605219c8a41cb79
Hugo Suastegui <hugo.suastegui.pineda@gamil.com>
2021-01-20 11:30:09 -0600

    Tested user update functionality on Postman correctly

--- controllers/users.js
+++ controllers/users.js
@@ -2,10 +2,9 @@ const User = require("../models/User");
 
 exports.updateUser = async (req, res) => {
   const { userId } = req.params;
-  rs;
   const { username } = req.body;
-  const userToUpdate = await User.findOne({ _id: userId });
-  userToUpdate.username = username;
-  userToUpdate.save();
-  res.status(200).json(userToUpdate);
+  const user = await User.findOne({ _id: userId });
+  user.username = username;
+  user.save();
+  res.status(200).json(user);
 };
