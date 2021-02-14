022c2dea60e379f68dc259f728efbe997b9591ae
Hugo Suastegui <hugo.suastegui.pineda@gamil.com>
2021-02-13 22:19:23 -0600

    Changed Subaccounts Model to include more accounts on its enum

--- models/Subaccount.js
+++ models/Subaccount.js
@@ -5,7 +5,15 @@ const subaccountSchema = new Schema(
     name: String,
     account: {
       type: String,
-      enum: ["Revenue", "COGS", "SG&A", "Taxes", "CapEX"],
+      enum: [
+        "Revenue",
+        "COGS",
+        "SG&A",
+        "Taxes",
+        "CapEX",
+        "Dividends",
+        "Retained Earnings",
+      ],
     },
     project: {
       type: Schema.Types.ObjectId,
