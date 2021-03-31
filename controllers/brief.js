6e292c7bdb355fc23394d4986bd64fc86c7ebfb2
Hugo Suastegui <hugo.suastegui.pineda@gamil.com>
2021-03-31 15:08:29 -0600

    Changed auth controllers to get Formik going smoothly

--- controllers/brief.js
+++ controllers/brief.js
@@ -97,12 +97,24 @@ exports.getAllConceptsByYear = async (req, res) => {
   });
 
   //Aquí tenemos que meterle GROSS MARGIN %
-  // data.splice(3, 0, {
-  //   name: "Gross Margin",
-  //   values: margin(data[3].values, data[0]).values,
-  // });
+  data.splice(3, 0, {
+    name: "Gross Margin",
+    subaccounts: [],
+    values: await margin(data[2].values, data[0].values),
+  });
+
   //Aquí tenemos que meterle EBITDA
+  data.splice(5, 0, {
+    name: "EBITDA",
+    subaccounts: [],
+    values: await sustract(data[2].values, data[4].values),
+  });
   //Aquí tenemos que meterle EBITDA %
+  data.splice(6, 0, {
+    name: "EBITDA Margin",
+    subaccounts: [],
+    values: await margin(data[5].values, data[0].values),
+  });
 
   res.status(200).json({ data });
 };
--- routes/auth.js
+++ routes/auth.js
@@ -62,7 +62,7 @@ router.post("/signup", (req, res, next) => {
     newUser
       .save()
       .then(() => {
-        res.status(200).json({ message: "ok" });
+        res.status(200).json({ message: "success" });
       })
       .catch((err) => {
         res.status(500).json({ message: "Something went wrong" });
